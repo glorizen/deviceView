b3de766d8c67a89c562fb8ecb95d7ffeb5c1dcfe
light <dev.trace.off@gmail.com>
2015-09-26 20:55:05 +0500

    HomePageTest (test_home_page.py) fails.

--- functional_tests/test_home_page.py
+++ functional_tests/test_home_page.py
@@ -9,8 +9,8 @@ class HomePageTest(FunctionalTest):
 	def test_home_page(self):
 	
 		# I visit the home page.
-		self.browser.get(self.server_url)
+		self.browser.get(self.live_server_url)
 		self.browser.set_window_size(1280, 720)
 		
 		# I notice a mobile product on the page.
-		self.assert(self.browser.find_element_by_id('id_mobi_product'))
+		self.assertTrue(self.browser.find_element_by_id('id_mobi_product'))
